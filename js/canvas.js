a008fdb92db66dccdc71b55ea44db928fd693f8b
Steven de Valk <s1129787@student.windesheim.nl>
2019-06-07 23:46:56 +0200

    init commit

---
+++ js/canvas.js
@@ -0,0 +1,93 @@
+// import utils from './utils'
+
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+var center = {
+    x: canvas.width / 2,
+    y: canvas.height / 2
+}
+
+const mouse = {
+    x: innerWidth / 2,
+    y: innerHeight / 2
+}
+
+const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
+
+var gravity = 0.5;
+// var friction = 0.99;
+var friction = 0.99;
+
+// Event Listeners
+addEventListener('mousemove', event => {
+    mouse.x = event.clientX
+    mouse.y = event.clientY
+})
+
+addEventListener('resize', () => {
+    canvas.width = innerWidth
+    canvas.height = innerHeight
+})
+
+addEventListener("click", function(){
+    init();
+});
+
+// Utility Functions
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function randomColor(colors) {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+
+// Objects
+function Ball(x, y, dx, dy, radius, color) {
+    this.x = x;
+    this.y = y;
+
+    this.dy = dy;
+    this.dx = dx;
+
+    this.radius = radius;
+    this.color = color;
+}
+
+function drawGrid(){
+    
+}
+
+Object.prototype.draw = function() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+
+    c.fill()
+
+    // c.stroke();
+
+    c.closePath()
+}
+
+Object.prototype.update = function() {
+   
+}
+
+// Implementation
+
+function init() {
+    
+}
+
+// Animation Loop
+function animate() {
+
+
+}
+
+init()
