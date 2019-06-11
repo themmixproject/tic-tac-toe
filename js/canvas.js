81ec3e23f597494b00244ad8871974b95b931688
Steven de Valk <s1129787@student.windesheim.nl>
2019-06-11 21:49:43 +0200

    drawGrid cleanup, coordinates drawgrid, gridSquare

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,7 @@
 // import utils from './utils'
 
+// global variables
+
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
@@ -27,7 +29,9 @@ var defaultStroke = 1;
 
 console.log(c.lineWidth);
 
+var gridSquareArray = [];
 
+var gridCoordinatesArray = [];
 
 // Event Listeners
 addEventListener('mousemove', event => {
@@ -53,6 +57,13 @@ addEventListener("click", function(){
 });
 
 // Utility Functions
+
+function resetBrush() {
+
+    c.lineWidth = 1;
+
+}
+
 function randomIntFromRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
@@ -69,53 +80,145 @@ function drawPath(x, y, x1, y1){
 }
 
 // Objects
-function Ball(x, y, dx, dy, radius, color) {
-    this.x = x;
-    this.y = y;
+// function Ball(x, y, dx, dy, radius, color) {
+//     this.x = x;
+//     this.y = y;
 
-    this.dy = dy;
-    this.dx = dx;
+//     this.dy = dy;
+//     this.dx = dx;
 
-    this.radius = radius;
-    this.color = color;
+//     this.radius = radius;
+//     this.color = color;
+// }
+
+class gridSquare{
+    constructor(x_tl, y_tl, x_tr, y_tr, x_bl, y_bl, x_br, y_br){
+
+        this.topLeft = {x:x_tl, y:y_tl};
+
+        this.topRight - {x:x_tr, y: Y_tr};
+        
+        this.bottomLeft = {x: x_bl, y: y_bl};
+
+        this.bottomRight = {x: x_br, y: y_br};
+
+        // this.x_tl = x_tl;
+        // this.y_tl = y_tl;
+
+        // this.x_tr = x_tr;
+        // this.y_tr = y_tr;
+
+        // this.x_bl = x_bl;
+        // this.y_bl = y_bl;
+
+        // this.x_br = x_br;
+        // this.y_br = y_br;
+
+    }
 }
+
 
 
 // draws the first grid
 function drawGrid(){
     
+    gridCoordinatesArray = [];
+
     c.lineWidth = 5;
 
     var gridPadding = 60;
 
     var x = center.x - gridPadding;
 
-    var height = gridPadding * 6;
+    var height = (gridPadding * 6) / 2;
 
     var y = center.y;
 
-    drawPath(x, y + ( height / 2 ),
-             x, y - ( height / 2 ));
+    drawPath(x, y + height,
+             x, y - height);
+
+
+    var leftVertical = {
+        description: "left vertical",
+        top: {
+            x: x,
+            y: y + height
+        },
+        bottom: {
+            x: x,
+            y: y - height
+        }
+    }
+
+    gridCoordinatesArray.push(leftVertical);
 
     var x = center.x + gridPadding;
 
-    drawPath(x, y + ( height / 2),
-             x, y - ( height / 2) );
+    
+    drawPath(x, y + height,
+             x, y - height );
+
+    var rightVertical = {
+        description: "right vertical",
+        top: {
+            x: x,
+            y: y + height
+        },
+        bottom: {
+            x: x,
+            y: y - height
+        }
+    }
+
+    gridCoordinatesArray.push(rightVertical);
 
     var x = center.x;
 
     var y = center.y - gridPadding;
 
-    var width = gridPadding * 6;
+    var width = height;
 
-    drawPath(x + ( width / 2 ), y,
-             x - ( width / 2 ), y);
+    drawPath(x + width, y,
+             x - width, y);
+
+    var topHorizontal = {
+        description: "top horizontal",
+        top: {
+            x: x + width,
+            y: y
+        },
+        bottom: {
+            x: x - width,
+            y: y
+        }
+    }
+
+    gridCoordinatesArray.push(topHorizontal)
 
     var y = center.y + gridPadding;
 
-    drawPath(x + width / 2, y,
-             x - width / 2, y);
+    drawPath(x + width, y,
+             x - width, y);
 
+    var bottomHorizontal = {
+        description: "bottom horizontal",
+        top: {
+            x: x + width,
+            y: y
+        },
+        bottom: {
+            x: x - width,
+            y: y
+        }
+    }
+
+    gridCoordinatesArray.push(bottomHorizontal)
+
+    console.log(gridCoordinatesArray);
+    
+
+   resetBrush();
+    
 }
 
 Object.prototype.draw = function() {
@@ -136,6 +239,8 @@ Object.prototype.update = function() {
 
 // Implementation
 
+var gridSquareArray = [];
+
 function init() {
 
     c.clearRect(0, 0, canvas.width, canvas.height);
@@ -150,4 +255,4 @@ function animate() {
 
 }
 
-init()
+init();
