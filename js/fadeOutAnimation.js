7748af645528d2191282c20b3a4cbc79c69652be
themmixproject <steven.devalk@gmail.com>
2020-10-14 12:01:06 +0200

    Got rid of old fadeoutReset() method

--- js/fadeOutAnimation.js
+++ js/fadeOutAnimation.js
@@ -7,57 +7,6 @@ var fadeDuration = 250;
 //  *|                                                    #
 // \#####################################################*/
 
-// function fadeOutReset(win, winArray){
-
-//     var iteration = 0;
-//     var easingVar;
-
-
-//     // var alphaIteration = 1;
-
-//     function animate(){
-
-//         console.log("fadeout iteration");
-
-//         c.clearRect(0,0,innerWidth,innerHeight);
-//         c.globalAlpha = 1;
-//         drawGrid();
-//         var easingVar = linear(iteration, 1, -1, toFps(fadeDuration));
-//         c.globalAlpha = easingVar;
-//         grid.forEach(function(item,y){
-//             item.forEach(function(piece,x){
-//                 if(piece==1){
-//                     c.globalAlpha = easingVar;
-//                     drawX(x,y);
-//                 }
-//                 else if(piece==2){
-//                     c.globalAlpha = easingVar;
-//                     drawO(x,y);
-//                 }
-//             });
-//         });
-
-//         if(win==true){
-//             c.globalAlpha = easingVar;
-//             drawWinLine(winArray);
-//         }
-
-//         if(iteration<toFps(fadeDuration)){
-//             iteration++;
-//             window.requestAnimationFrame(animate);
-//         }
-
-//         if(iteration==toFps(fadeDuration)){
-//             resetBrush();
-//         }
-
-//     }
-
-//     animate();
-    
-//     c.globalAlpha = 1;
-// }
-
 function fadeOutReset(win, winArray){
     
     var secondsPassed = 0;
