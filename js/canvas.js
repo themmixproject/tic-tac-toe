d0e184b0803f971c78d34a144d7b1dfa93cd4f21
themmixproject <steven.devalk@gmail.com>
2021-07-14 00:04:38 +0200

    added computerPlayer.getTargetFromBaseIndex() method

--- js/canvas.js
+++ js/canvas.js
@@ -290,17 +290,73 @@ var computerPlayer = {
         [6, 4, 2]
     ],
     currentTargetIndex: 0,
+    potentialBaseIndexes: [],
     init: function(){
         computerPlayer.getNewTargetCombination();
     },
     getNewTargetCombination: function(){
         computerPlayer.updatePotentialTargetCombinations();
-        
-        var targetIndex = Math.floor(Math.random() * computerPlayer.potentialTargets.length);
-        var target = computerPlayer.potentialTargets[targetIndex];
+
+        var target = [];
+        if(computerPlayer.potentialBaseIndexes.length > 0){
+            target = computerPlayer.getTargetFromBaseIndex();
+            
+            console.log("base target: " + target);
+        }
+
+        var noBaseTargetFound = (target.length === 0);
+        if(noBaseTargetFound){
+            var targetIndex = Math.floor(Math.random() * computerPlayer.potentialTargets.length);
+            target = computerPlayer.potentialTargets[targetIndex];
+        }
 
         computerPlayer.setTargetCombination(target);
     },
+    getTargetFromBaseIndex: function(){
+        var selectedCombination = [];
+
+        computerPlayer.potentialBaseIndexes.forEach(function(baseIndex, itemIndex){
+            var baseCombinations = computerPlayer.getCombinationsFromBase(baseIndex);
+            var potentialBaseCombinations = computerPlayer.filterPossibleCombinations(baseCombinations);
+            
+            // console.log("base index:" + baseIndex + "\nbase combinations:" +  baseCombinations);
+            // console.log("potential base combinations:" + potentialBaseCombinations);
+
+            if(potentialBaseCombinations.length > 0 && selectedCombination.length === 0){
+                var selectedIndex = Math.floor(Math.random() * potentialBaseCombinations.length);
+                console.log("potential combinations:" + potentialBaseCombinations + "\nselected index: " + selectedIndex);
+
+                selectedCombination = potentialBaseCombinations[selectedIndex];
+            }
+            else{
+                computerPlayer.potentialBaseIndexes.splice(itemIndex, 1);
+            }
+
+            console.log("selected combination: " + selectedCombination);
+        });
+
+        return selectedCombination;
+    },
+    getCombinationsFromBase: function(baseIndex){
+        var baseCombinations = [];
+        computerPlayer.winCombinationIndexes.forEach(function(combination){
+            var hasIndex = combination.indexOf(baseIndex) > -1;
+            if(hasIndex)
+                baseCombinations.push(combination);
+        })
+
+        return baseCombinations;
+    },
+    filterPossibleCombinations: function(possibleCombinations){
+        var potentialCombinations = [];
+        possibleCombinations.forEach(function(combination){
+            if(computerPlayer.combinationHasPotential(combination))
+                potentialCombinations.push(combination);
+        })
+
+        return potentialCombinations;
+    },
+
     setTargetCombination: function(target){
         computerPlayer.currentTarget = target;
         computerPlayer.checkedPotentialTargets.push(target);
@@ -313,7 +369,6 @@ var computerPlayer = {
     },
     filterPlacedPieces: function(){
         computerPlayer.currentTarget.forEach(function(pieceIndex, indexInArray){
-            console.log("filter: " + pieceIndex + " " + JSON.stringify(computerPlayer.placedPieces) + "\nIs in array:" + (computerPlayer.placedPieces.indexOf(pieceIndex) > -1) );
             if(computerPlayer.placedPieces.indexOf(pieceIndex) > -1){
 
                 computerPlayer.currentTarget.splice(indexInArray, 1);
@@ -331,8 +386,6 @@ var computerPlayer = {
         });
         
         computerPlayer.filterPotentialTargets();
-
-        console.log( JSON.stringify(computerPlayer.potentialTargets) );
     },
     combinationHasPotential: function(combination){
         var hasPotential = true;
@@ -364,9 +417,10 @@ var computerPlayer = {
         var turnCoordinates = computerPlayer.getTurnCoordinates();
         var turnIndex = convertBoardCoordinateToIndex(turnCoordinates[0], turnCoordinates[1]);
         
-        console.log("Turncoordinates: " + turnCoordinates);
+        // console.log("Turncoordinates: " + turnCoordinates);
 
         computerPlayer.placedPieces.push(turnIndex);
+        computerPlayer.potentialBaseIndexes.push(turnIndex);
 
         // // update game state
         gameBoard[ turnCoordinates[0] ][ turnCoordinates[1] ] = currentPlayer.piece;
