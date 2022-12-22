0026c589124cf1f41e485c0ba5dd135d3c2989fe
themmixproject <steven.devalk@gmail.com>
2022-12-22 12:14:27 +0100

    Reordered code to fit overal flow better

--- js/canvas.js
+++ js/canvas.js
@@ -37,204 +37,6 @@
  * THEME : #D9695F #FFB4A8 #EFCDBF #4F9BA8 #2D3742
  */
 
-// monkey patches
-window.requestAnimationFrame = (function () {
-    return (
-        window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        function (callback) {
-            window.setTimeout(callback, 1000 / 60);
-        }
-    );
-})();
-window.cancelAnimationFrame = (function () {
-    return (
-        window.cancelAnimationFrame ||
-        window.webkitCancelAnimationFrame ||
-        window.mozCancelAnimationFrame ||
-        function (timPtr) {
-            window.clearTimeout(timPtr);
-        }
-    );
-})();
-
-var styles = {
-    defaultThickness: 10,
-    cross: {
-        color: "#4F9BA8",
-        cap: "round",
-        thickness: 10,
-    },
-    circle: {
-        color: "#D9695F",
-        cap: "round",
-        thickness: 10,
-    },
-    grid: {
-        color: "#2D3742",
-        thickness: 10,
-        cap: "round",
-    },
-    winLine: {
-        color: "#FFEDE0",
-        thickness: 10,
-        cap: "round",
-    },
-};
-
-var canvasBackgroundColor = "#EFCDBF";
-
-/*#####################################################\
- *|                                                    #
- *| 3. Canvas properties                               #
- *|                                                    #
-\#####################################################*/
-
-// setting up canvas
-var canvas = document.getElementById("canvas");
-canvas.style.backgroundColor = canvasBackgroundColor;
-
-var canvasContext = canvas.getContext("2d");
-
-var devicePixelRatio = Math.ceil(window.devicePixelRatio || 1);
-var backingStoreRatio =
-    canvasContext.webkitBackingStorePixelRatio ||
-    canvasContext.mozBackingStorePixelRatio ||
-    canvasContext.msBackingStorePixelRatio ||
-    canvasContext.oBackingStorePixelRatio ||
-    canvasContext.backingStorePixelRatio ||
-    1;
-
-devicePixelRatio = devicePixelRatio / backingStoreRatio;
-canvasContext.setTransform(devicePixelRatio, 0, 0, devicePixelRatio, 0, 0);
-
-canvasCenter = {
-    x: 0,
-    y: 0,
-};
-
-function loadStyle(style) {
-    canvasContext.lineWidth = style.thickness;
-    canvasContext.lineCap = style.cap;
-    canvasContext.strokeStyle = style.color;
-}
-
-function resetContextStyleToDefault() {
-    canvasContext.lineWidth = 1;
-    canvasContext.lineCap = "butt";
-    canvasContext.strokeStyle = "#000000";
-}
-
-grid = {
-    margin: 15 * devicePixelRatio,
-    celPadding: 20 * devicePixelRatio,
-    lineLength: 350.5 * devicePixelRatio,
-};
-grid.sectionLength = grid.lineLength / 3;
-grid.width = grid.lineLength + grid.margin;
-grid.height = grid.width;
-grid.maxHeightWidth = 350.5;
-grid.minHeighWidth = 200;
-grid.maxWidth = 350.5;
-grid.minWidth = 200;
-grid.maxHeight = grid.maxWidth;
-grid.minHeight = grid.minWidth;
-grid.topLeftCoordinates = {
-    x: canvasCenter.x - grid.sectionLength * 1.5,
-    y: canvasCenter.y - grid.sectionLength * 1.5,
-};
-grid.setHeightAndWidth = function (heightWidth) {
-    grid.width = heightWidth;
-
-    //the grid is a square, so height and width are the same
-    grid.height = grid.width;
-};
-
-function scaleCanvasOnLoad() {
-    updateCanvasAttributes();
-    updateGridSize();
-    updateGridAttributes();
-}
-
-var gameBoard = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-];
-
-var computerHasMoved = false;
-var playerCanClick = true;
-
-var game = {
-    hasBeenWon: false,
-    hasEnded: false,
-    winningCombination: [],
-    endFunctionHasBeenCalled: false,
-};
-
-var winCombinations = [
-    // horizontal
-    [
-        [0, 0],
-        [0, 1],
-        [0, 2],
-    ],
-    [
-        [1, 0],
-        [1, 1],
-        [1, 2],
-    ],
-    [
-        [2, 0],
-        [2, 1],
-        [2, 2],
-    ],
-
-    // vertical
-    [
-        [0, 0],
-        [1, 0],
-        [2, 0],
-    ],
-    [
-        [0, 1],
-        [1, 1],
-        [2, 1],
-    ],
-    [
-        [0, 2],
-        [1, 2],
-        [2, 2],
-    ],
-
-    // diagonal
-    [
-        [0, 0],
-        [1, 1],
-        [2, 2],
-    ],
-    [
-        [0, 2],
-        [1, 1],
-        [2, 0],
-    ],
-];
-
-var currentPlayer;
-
-/**
- * resets canvas brush to default values
- */
-function resetBrush() {
-    c.lineWidth = 1;
-    c.strokeStyle = "black";
-    c.fillStyle = "black";
-    c.lineCap = "butt";
-
-    c.globalAlpha = 1;
-}
-
 function logGameBoard() {
     var logString = "";
 
@@ -269,6 +71,204 @@ function rotateBoard() {
     return rotatedRows;
 }
 
+
+
+// monkey patches
+window.requestAnimationFrame = (function () {
+    return (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        }
+    );
+})();
+window.cancelAnimationFrame = (function () {
+    return (
+        window.cancelAnimationFrame ||
+        window.webkitCancelAnimationFrame ||
+        window.mozCancelAnimationFrame ||
+        function (timPtr) {
+            window.clearTimeout(timPtr);
+        }
+    );
+})();
+
+function multiDimensionalArrayHasArray(multiArray, checkArray) {
+    var containsItem = false;
+    multiArray.forEach(function (item) {
+        if (arrayEquals(item, checkArray)) containsItem = true;
+    });
+    return containsItem;
+}
+function arrayEquals(a, b) {
+    return (
+        Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index])
+    );
+}
+
+
+var styles = {
+    defaultThickness: 10,
+    cross: {
+        color: "#4F9BA8",
+        cap: "round",
+        thickness: 10,
+    },
+    circle: {
+        color: "#D9695F",
+        cap: "round",
+        thickness: 10,
+    },
+    grid: {
+        color: "#2D3742",
+        thickness: 10,
+        cap: "round",
+    },
+    winLine: {
+        color: "#FFEDE0",
+        thickness: 10,
+        cap: "round",
+    },
+};
+
+function loadStyle(style) {
+    canvasContext.lineWidth = style.thickness;
+    canvasContext.lineCap = style.cap;
+    canvasContext.strokeStyle = style.color;
+}
+
+
+var canvasBackgroundColor = "#EFCDBF";
+
+
+/*#####################################################\
+*|                                                    #
+*| 3. Canvas properties                               #
+*|                                                    #
+\#####################################################*/
+
+// setting up canvas
+var canvas = document.getElementById("canvas");
+canvas.style.backgroundColor = canvasBackgroundColor;
+
+var canvasContext = canvas.getContext("2d");
+
+var devicePixelRatio = Math.ceil(window.devicePixelRatio || 1);
+var backingStoreRatio =
+    canvasContext.webkitBackingStorePixelRatio ||
+    canvasContext.mozBackingStorePixelRatio ||
+    canvasContext.msBackingStorePixelRatio ||
+    canvasContext.oBackingStorePixelRatio ||
+    canvasContext.backingStorePixelRatio ||
+    1;
+
+devicePixelRatio = devicePixelRatio / backingStoreRatio;
+canvasContext.setTransform(devicePixelRatio, 0, 0, devicePixelRatio, 0, 0);
+canvasContext.scale(devicePixelRatio, devicePixelRatio)
+
+canvasCenter = {
+    x: 0,
+    y: 0,
+};
+
+function resetContextStyleToDefault() {
+    canvasContext.lineWidth = 1;
+    canvasContext.lineCap = "butt";
+    canvasContext.strokeStyle = "#000000";
+}
+
+grid = {
+    margin: 15,
+    celPadding: 20,
+    lineLength: 350.5 ,
+};
+grid.sectionLength = grid.lineLength / 3;
+grid.width = grid.lineLength + grid.margin;
+grid.height = grid.width;
+grid.maxHeightWidth = 350.5;
+grid.minHeighWidth = 200;
+grid.maxWidth = 350.5;
+grid.minWidth = 200;
+grid.maxHeight = grid.maxWidth;
+grid.minHeight = grid.minWidth;
+grid.topLeftCoordinates = {
+    x: canvasCenter.x - grid.sectionLength * 1.5,
+    y: canvasCenter.y - grid.sectionLength * 1.5,
+};
+grid.setHeightAndWidth = function (heightWidth) {
+    grid.width = heightWidth;
+
+    //the grid is a square, so height and width are the same
+    grid.height = grid.width;
+};
+
+
+var gameBoard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+];
+function convertBoardCoordinateToIndex(x, y) {
+    // 3 because it's a 3x3 grid
+    return y * 3 + x;
+}
+
+function convertIndexToBoardCoordinate(index) {
+    var x = index % 3;
+    var y = Math.floor((index / 3) % 3);
+
+    return [x, y];
+}
+
+function convertBoardToCanvasCoordinates(x, y) {
+    var canvasX = x * grid.sectionLength + grid.topLeftCoordinates.x;
+    var canvasY = y * grid.sectionLength + grid.topLeftCoordinates.y;
+
+    return { x: canvasX, y: canvasY };
+}
+
+var computerHasMoved = false;
+var playerCanClick = true;
+
+var game = {
+    hasBeenWon: false,
+    hasEnded: false,
+    winningCombination: [],
+    endFunctionHasBeenCalled: false,
+};
+
+var winCombinations = [
+    // horizontal
+    [ [0, 0], [0, 1], [0, 2], ],
+    [ [1, 0], [1, 1], [1, 2], ],
+    [ [2, 0], [2, 1], [2, 2], ],
+
+    // vertical
+    [ [0, 0], [1, 0], [2, 0], ],
+    [ [0, 1], [1, 1], [2, 1], ],
+    [ [0, 2], [1, 2], [2, 2], ],
+
+    // diagonal
+    [ [0, 0], [1, 1], [2, 2], ],
+    [ [0, 2], [1, 1], [2, 0], ],
+];
+
+var currentPlayer;
+var players = {
+    humanPlayer: {
+        piece: "X",
+        canInteract: true,
+    },
+    computerPlayer: {
+        piece: "O",
+    },
+};
+
 function addEvents() {
     if (isMobileDevice()) addMobileEvents();
     else addDesktopEvents();
@@ -294,44 +294,8 @@ function canvasClickEvent(event) {
     canvasInteraction(clientXY);
 }
 
-var players = {
-    humanPlayer: {
-        piece: "X",
-        canInteract: true,
-    },
-    computerPlayer: {
-        piece: "O",
-    },
-};
 
-function convertBoardCoordinateToIndex(x, y) {
-    // 3 because it's a 3x3 grid
-    return y * 3 + x;
-}
 
-function convertIndexToBoardCoordinate(index) {
-    var x = index % 3;
-    var y = Math.floor((index / 3) % 3);
-
-    return [x, y];
-}
-
-function arrayEquals(a, b) {
-    return (
-        Array.isArray(a) &&
-        Array.isArray(b) &&
-        a.length === b.length &&
-        a.every((val, index) => val === b[index])
-    );
-}
-
-function multiDimensionalArrayHasArray(multiArray, checkArray) {
-    var containsItem = false;
-    multiArray.forEach(function (item) {
-        if (arrayEquals(item, checkArray)) containsItem = true;
-    });
-    return containsItem;
-}
 
 function shuffleArray(array) {
     var currentIndex = array.length,
@@ -374,6 +338,19 @@ function canvasInteraction(clientCoordinates) {
         }
     }
 }
+function hasCollisionWithGridCel(clientXY, celXY) {
+    var startX = celXY.x;
+    var endX = celXY.x + grid.sectionLength;
+    var startY = celXY.y;
+    var endY = celXY.y + grid.sectionLength;
+
+    var hasXCollision = clientXY.x >= startX && clientXY.x <= endX;
+    var hasYCollision = clientXY.y >= startY && clientXY.y <= endY;
+
+    return hasXCollision && hasYCollision;
+}
+
+
 
 function playerTurn(x, y) {
     players.humanPlayer.canInteract = false;
@@ -476,17 +453,6 @@ function checkIfGameHasTied() {
     if (pieceCounter === 9) game.hasEnded = true;
 }
 
-function hasCollisionWithGridCel(clientXY, celXY) {
-    var startX = celXY.x;
-    var endX = celXY.x + grid.sectionLength;
-    var startY = celXY.y;
-    var endY = celXY.y + grid.sectionLength;
-
-    var hasXCollision = clientXY.x >= startX && clientXY.x <= endX;
-    var hasYCollision = clientXY.y >= startY && clientXY.y <= endY;
-
-    return hasXCollision && hasYCollision;
-}
 
 function drawCrossOnCanvas(boardX, boardY) {
     loadStyle(styles.cross);
@@ -535,12 +501,7 @@ function drawCircleOnCanvas(boardX, boardY) {
     canvasContext.stroke();
 }
 
-function convertBoardToCanvasCoordinates(x, y) {
-    var canvasX = x * grid.sectionLength + grid.topLeftCoordinates.x;
-    var canvasY = y * grid.sectionLength + grid.topLeftCoordinates.y;
 
-    return { x: canvasX, y: canvasY };
-}
 
 function windowResizeEvent() {
     clearCanvas();
@@ -579,6 +540,13 @@ function updateCanvasAttributes() {
         x: canvas.width / 2,
         y: canvas.height / 2,
     };
+}
+
+
+function scaleCanvasOnLoad() {
+    updateCanvasAttributes();
+    updateGridSize();
+    updateGridAttributes();
 }
 
 function updateGridSize() {
@@ -749,6 +717,6 @@ function drawWinLineOnCanvas() {
 
 // Implementation
 
-addEvents();
+// addEvents();
 scaleCanvasOnLoad();
-drawGridOnCanvas();
+// drawGridOnCanvas();
